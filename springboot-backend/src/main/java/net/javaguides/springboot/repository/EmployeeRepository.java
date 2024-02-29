d09ee21154d2951b474b809d218029df14abcab2
Praveen Patidar <baskrpraveen@gmail.com>
2024-02-29 22:45:59 +0530

    Update Docker files

--- springboot-backend/src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
@@ -8,5 +8,5 @@ import net.javaguides.springboot.model.Employee;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long>{
 
-    Employee findBYfirstName(String firstName);
+   // Employee findBYfirstName(String firstName);
 }
